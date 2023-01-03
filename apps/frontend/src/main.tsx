39b0f0199d03ffed6c08ad9c26678e7d81920e07
yakovsinwani <yakovsinwani@gmail.com>
2023-01-03 19:55:15 +0200

    init react query

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,33 +2,20 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './app/app';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 5,
-      retryDelay: 2000,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+/** Create here new QueryClient, and render the "QueryClientProvider" with this client.*/
 
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </StrictMode>
 );
